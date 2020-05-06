61eb311cb499f1de4c1312d34b68c3161eefbf83
joao.goncalves <joao.goncalves@morphis-tech.com>
2020-05-06 15:31:51 +0100

    código com erros

---
+++ Teste.java
@@ -0,0 +1,16 @@
+package cgd.gh.online;
+
+
+public abstract class Teste  {
+    String testecode;
+
+    String testecodeguru = 123;
+    
+    if(testecode = testecodeguru) {
+    	
+    	testecode = "abc"
+    	
+    }
+    
+    }
+}
