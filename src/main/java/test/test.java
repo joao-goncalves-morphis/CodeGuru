50782c06911e5ec6375ba8da1a3b72e5056cbb1c
joao.goncalves <joao.goncalves@morphis-tech.com>
2020-05-27 14:27:44 +0100

    adicionada classe de teste

---
+++ src/main/java/test/test.java
@@ -0,0 +1,16 @@
+package cgd.gh.online;
+
+
+public abstract class test  {
+    String testcode;
+
+    String testecodeguru = 123;
+    
+    if(testcode = testecodeguru) {
+    	
+    	testcode = "abc"
+    	
+    }
+    
+    }
+}
